2cd6252d8f7004cab9880a06ad56695d1ad2851a
Jishnumoh <jishnumohan292@gmail.com>
2024-05-04 22:03:28 +0530

    commit

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var video = document.getElementById('video-bg');
-
-    // Pause the video when it ends
-    video.addEventListener('ended', function () {
-        video.pause();
-    });
-});
